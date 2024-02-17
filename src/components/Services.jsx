2149858b9e1dd228bc4f853b636746725b679d65
LincolinAngaya <lincolinangaya5626@gmail.com>
2024-02-17 12:47:25 +0300

    modify listing

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { GoCheckbox } from "react-icons/go";
 import { servicesData } from '../..';
 
 import services from '../assets/service.png';
@@ -26,21 +27,34 @@ function Services() {
     </div>
     
 
-    <div className="mt-12 ml-9 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-11">
+    <div className=" mt-12 ml-9 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-11">
     <div className="mb-8">
       <h1 className="text-3xl sm:text-3xl md:text-5xl font-bold mb-4">Other Services</h1>
       <p className="mb-4">
         Amauwa offers the best landscaping services in Nairobi. We aim to leave your compound beautiful and with a long-lasting impact.
       </p>
-      <div>
-        <ul className="list-disc pl-4">
-          <li>Designing baskets</li>
-          <li>Propagation</li>
-          <li>Designing baskets</li>
-          <li>Cutting edges</li>
-        </ul>
-      </div>
-      <button className="py-2 px-4 bg-customGreen text-white rounded-full hover:bg-green-700 transition duration-300">Get Quote</button>
+      <div className=''>
+      <ul className="list-disc pl-4 font-roboto">
+        <li className="flex items-center mb-2"  >  <GoCheckbox className="text-green-500 h-5 w-5 mr-2" />
+      
+          Designing baskets
+        </li>
+        <li className="flex items-center mb-2">
+        <GoCheckbox className="text-green-500 h-5 w-5 mr-2" />
+          Propagation
+        </li>
+        <li className="flex items-center mb-2">
+        <GoCheckbox className="text-green-500 h-5 w-5 mr-2" />
+          Designing baskets
+        </li>
+        <li className="flex items-center">
+        <GoCheckbox className="text-green-500 h-5 w-5 mr-2" />
+          Cutting edges
+        </li>
+      </ul>
+    </div>
+    
+      <button className="py-2 mt-4 px-4 bg-customGreen text-white rounded-full hover:bg-green-700 transition duration-300">Get Quote</button>
     </div>
     <div className="mb-8">
       <img src={services} alt="services" className="w-full h-auto" />
