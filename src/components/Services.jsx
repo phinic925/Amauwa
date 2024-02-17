cf71690e5927331ba0e29bf2810129a3e3941056
LincolinAngaya <lincolinangaya5626@gmail.com>
2024-02-17 10:40:40 +0300

    use mapping to import the imGES

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import frontyard from '../assets/frontyard.png';
-import kitchen from '../assets/kitchen.png';
-import maintenance from '../assets/maintenance.png';
-import interior from '../assets/interior.png';
+import { servicesData } from '../..';
+
 import services from '../assets/service.png';
 
 function Services() {
@@ -11,29 +9,22 @@ function Services() {
       <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold mb-6">What We Offer</h1>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
-        <div className="mb-8">
-          <img src={frontyard} alt="FrontYard Landscaping" className="mb-4 w-full h-auto" />
-          <h2 className="text-xl font-semibold">FrontYard Landscaping</h2>
+      {servicesData.map((service) => (
+        <div key={service.id} className="mb-8 relative">
+          <img src={service.image} alt={service.alt} className="mb-4 w-full h-40 object-cover" />
+          <h2 className="text-xl font-bold text-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+            {service.title}
+          </h2>
         </div>
-        <div className="mb-8">
-          <img src={kitchen} alt="Kitchen Garden" className="mb-4 w-full h-auto" />
-          <h2 className="text-xl font-semibold">Kitchen Garden</h2>
-        </div>
-        <div className="mb-8">
-          <img src={maintenance} alt="Garden Maintenance" className="mb-4 w-full h-auto" />
-          <h2 className="text-xl font-semibold">Garden Maintenance</h2>
-        </div>
-        <div className="mb-8">
-          <img src={interior} alt="Interior Plants" className="mb-4 w-full h-auto" />
-          <h2 className="text-xl font-semibold">Interior Plants</h2>
-        </div>
-      </div>
+      ))}
+    </div>
+    
 
       <div className="mt-12 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
         <div className="mb-8">
           <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold mb-4">Other Services</h1>
           <p className="mb-4">
-            L'Amauwa offers the best landscaping services in Nairobi. We aim to leave your compound beautiful and with a long-lasting impact.
+          Amauwa offers the best landscaping services in Nairobi. We aim to leave your compound beautiful and with a long-lasting impact.
           </p>
           <div>
             <ul className="list-disc pl-4">
